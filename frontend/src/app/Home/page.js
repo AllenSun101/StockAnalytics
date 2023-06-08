96dfc222c326d1a2738982ecf36f278668952da8
AllenSun101 <allensun21527@gmail.com>
2023-06-07 19:21:57 -0500

    restructured project and added axios test code

--- frontend/src/app/Home/page.js
+++ frontend/src/app/Home/page.js
@@ -1,5 +1,19 @@
+'use client'
+
+import axios from "axios";
+import React from "react";
+
 export default function Home(){
+    const [message, setMessage] = React.useState(null);
+    axios.get(
+        'http://localhost:3001/database'
+    )
+    .then(
+        response => {
+            setMessage(response.data);
+        }
+    )
     return(
-        <h1> Hello! </h1>
+        <h1>{message}</h1>
     )
 }
